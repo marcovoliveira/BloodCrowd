55e51e94a0a407ced91917aa8f62da993299ca64
Oleksandr Gevko <5150231@my.ipleiria.pt>
2017-10-12 21:51:19 +0100

    aaa

--- Interface/AddNewDonator.cs
+++ Interface/AddNewDonator.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Xml;
 
 namespace Interface
@@ -16,7 +15,7 @@ namespace Interface
     float kilograms, float centimeters, String guid, double latitude, double longitude, XmlDocument doc)
         {
             XmlElement Donator = doc.CreateElement("Donator");
-            MessageBox.Show(city);
+
             return Donator;
         }
     }
