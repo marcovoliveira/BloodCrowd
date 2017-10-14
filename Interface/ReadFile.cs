2f5540e055c5214375cc2622c53551cd6db13d5e
rezken1 <marcooliveirapias@gmail.com>
2017-10-15 00:04:14 +0100

    Guardar em XML completamente funcional e sem espaços nem caracteres especiais, adicionado Lista de Dadores

--- Interface/ReadFile.cs
+++ Interface/ReadFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
@@ -16,46 +17,59 @@ namespace Interface
             OpenFileDialog o = new OpenFileDialog();
             o.ShowDialog();
             o.DefaultExt = "*.txt";
+
             String receber = File.ReadAllText(o.FileName);
+
             char d = '\n';
             char f = '|';
+
             String[] dadosPorEnter = receber.Split(d);
-            int dadosSize = dadosPorEnter.Length;
+            int dadosPorEnterSize = dadosPorEnter.Length;
+
             String[] dado;
-            int i;
+            
 
             List<String> dados = new List<String>();
             XmlDocument doc = new XmlDocument();
             XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", null, null);
             doc.AppendChild(dec);
+            XmlElement root = doc.CreateElement("DonatorsList"); // Criar um root onde os Elementos Donators irão ser introduzidos
+            doc.AppendChild(root);
 
-            for (i = 1; i < dadosSize; i++)
+            for (int i = 1; i < dadosPorEnterSize; i++)
             {
-                dados.Add(dadosPorEnter[i]); //adicionar pessoa
-               
+                dados.Add(dadosPorEnter[i]); //adicionar pessoa          
             }
 
-            int dadosSize2 = dados.Count;
-            for (i = 0; i < dadosSize2; i++)
+
+            for (int j = 0; j < dados.Count-1; j++) //dados -1 para não contar o ultimo elemento da lista dados que esta em branco 
             {
                 
-                dado = dados[i].Split(f); //dividir pessoa por pipe
-                AddNewDonator.AddDonator(dado[0], dado[1], dado[2], dado[3],
-                       dado[4], dado[5], dado[6], dado[7], dado[8],
-                       dado[9], dado[10], dado[11], dado[12], dado[13],
-                        dado[14], dado[15], dado[16], dado[17], dado[18],
-                       dado[19],dado[20], dado[21], dado[22],
-                        dado[23], doc);
-                
+                dado = dados[j].Split(f); //dividir pessoa por pipe
 
-                
-            }
-            doc.AppendChild(doc);
-            MessageBox.Show(doc.InnerXml);
-           
+                for (int k = 0; k < dado.Length; k++)   //remove os espaços e o caracter especial no 1
+                {
+                   dado[k] = Regex.Replace(dado[k], @"\s+", ""); //remove espaços
+                   dado[0] = Regex.Replace(dado[0], @"§", "");   //remove caracter especial que apenas aparece no primeiro elemento
+                }
+
+                //chamada do Metodo AddDonator a cada iteração que ira introduzir um Elemento Donator na DonatorsList
+                root.AppendChild(AddNewDonator.AddDonator(dado[0], dado[1], dado[2], dado[3],
+                    dado[4], dado[5], dado[6], dado[7], dado[8],
+                    dado[9], dado[10], dado[11], dado[12], dado[13],
+                    dado[14], dado[15], dado[16], dado[17], dado[18],
+                    dado[19], dado[20], dado[21], dado[22],
+                    dado[23], doc));
+
+
+
+
 
             }
+            
+            MessageBox.Show(doc.OuterXml);
+
+            doc.Save(@"teste2.xml"); //guardar o documento XML
         }
-
-        
+        }      
     }
