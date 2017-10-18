8e08636894c1318b6b683f2f11d437d366530f41
rezken1 <marcooliveirapias@gmail.com>
2017-10-18 12:14:29 +0100

    retry

---
+++ Interface/BloodDonator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class BloodDonator
+    {
+        public BloodDonator(int number, String sexo, String firstName, String lastName, String streetAddress,
+            String city, String statefull, String zipCode, String eMail, String userName, String password,
+            long telephoneNumber,
+            String mothersMaiden, DateTime birthDay, String age, String occupation, String company, String vehicle,
+            String bloodType,
+            String kilograms, String centimeters, String guid, String latitude, String longitude)
+
+        {
+            
+
+        }
+
+    }
+}
