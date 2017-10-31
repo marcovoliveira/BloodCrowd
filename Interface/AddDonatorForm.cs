47d8e8675ccdc35dc515126a2b8099689ba2977a
Oleksandr Gevko <5150231@my.ipleiria.pt>
2017-10-31 14:36:28 +0000

    marco é feio

--- Interface/AddDonatorForm.cs
+++ Interface/AddDonatorForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Interface
 {
@@ -15,6 +16,26 @@ namespace Interface
         public AddDonatorForm()
         {
             InitializeComponent();
+
+            //Conteudo da comboBox do sexo
+            genreComboBox.Items.Add("male");
+            genreComboBox.Items.Add("female");
+            genreComboBox.SelectedIndex = 1;
+
+            //Conteudo da comboBox do grupo sanguíneo
+            bloodTypeComboBox.Items.Add("A+");
+            bloodTypeComboBox.Items.Add("A-");
+            bloodTypeComboBox.Items.Add("B+");
+            bloodTypeComboBox.Items.Add("B-");
+            bloodTypeComboBox.Items.Add("AB+");
+            bloodTypeComboBox.Items.Add("AB-");
+            bloodTypeComboBox.Items.Add("O+");
+            bloodTypeComboBox.Items.Add("O-");
+            bloodTypeComboBox.SelectedIndex = 0;
+
+
+
+
         }
 
         private void AddDonatorForm_Load(object sender, EventArgs e)
@@ -61,5 +82,47 @@ namespace Interface
         {
 
         }
+
+        private void genreComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void AddDonatorButton_Click(object sender, EventArgs e)
+        {
+            
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"BaseDados.xml");
+
+
+
+
+            XmlNode person = doc.SelectSingleNode("//Donators[last()]/@id");
+
+            String genero = genreComboBox.SelectedItem.ToString();
+            String firstName = firstNameTextBox.Text;
+            String lastName = lastNameTextBox.Text;
+            String streetAddress = streetAddressTextBox.Text;
+            String city = cityTextBox.Text;
+            String statefull = stateFullTextBox.Text;
+            String zipCode = zipCodeTextBox.Text;
+            String email = emailTextBox.Text;
+            String username = userNameTextBox.Text;
+            String telephone = telephoneTextBox.Text;
+            String mothersMaiden = mothersMaidenTextBox.Text;
+            String birthday = birthDaydateTimePicker.Text;
+            String occupation = occupationTextBox.Text;
+            String company = companyTextBox.Text;
+            String vehicle = vehicleTextBox.Text;
+            String bloodType = bloodTypeComboBox.SelectedItem.ToString();
+            String kilograms = kilogramsTextBox.Text;
+            String centimeters = centimetersTextBox.Text;
+            MessageBox.Show(Convert.ToString(person.InnerText));
+            // AddNewDonator.AddDonator();
+
+            //doc.Save(@"BaseDados.xml");
+        }
     }
 }
+    
