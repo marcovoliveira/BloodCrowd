969809bfaaa39b354f0c3b5f9402ce5061fd12df
Oleksandr Gevko <5150231@my.ipleiria.pt>
2017-12-05 11:14:58 +0000

    Criação da classe blooddonator no serviço

--- Socorro/IService1.cs
+++ Socorro/IService1.cs
@@ -39,5 +39,153 @@ namespace Socorro
                          String mothersMaiden, String birthDay, String age, String occupation, String company, String vehicle, String bloodType,
                          String kilograms, String centimeters, String guid, String latitude, String longitude,XmlDocument doc);
 
+        
+       
     }
+    //Criação da classe BloodDonator
+    [DataContract]
+    public class BloodDonator
+    {
+        public BloodDonator(int number, String sexo, String firstName, String lastName, String streetAddress,
+                String city, String statefull, String zipCode, String eMail, String userName, String password,
+                long telephoneNumber,
+                String mothersMaiden, DateTime birthDay, int age, String occupation, String company, String vehicle,
+                String bloodType,
+                double kilograms, double centimeters, String guid, String latitude, String longitude, double imc)
+        {
+            this.Number = number;
+            this.Sexo = sexo;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.StreetAddress = streetAddress;
+            this.City = city;
+            this.Statefull = statefull;
+            this.ZipCode = zipCode;
+            this.EMail = eMail;
+            this.UserName = userName;
+            this.Password = password;
+            this.TelephoneNumber = telephoneNumber;
+            this.MothersMaiden = mothersMaiden;
+            this.BirthDay = birthDay;
+            this.Age = age;
+            this.Occupation = occupation;
+            this.Company = company;
+            this.Vehicle = vehicle;
+            this.BloodType = bloodType;
+            this.Kilograms = kilograms;
+            this.Centimeters = centimeters;
+            this.Guid = guid;
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+            this.Imc = imc;
+            
+        }
+        [DataMember]
+        public int Number { get; private set; }
+
+        [DataMember]
+        public String Sexo { get; private set; }
+
+        [DataMember]
+        public String FirstName { get; private set; }
+
+        [DataMember]
+        public String LastName { get; private set; }
+
+        [DataMember]
+        public String StreetAddress { get; private set; }
+
+        [DataMember]
+        public String City { get; private set; }
+
+        [DataMember]
+        public String Statefull { get; private set; }
+
+        [DataMember]
+        public String ZipCode { get; private set; }
+
+        [DataMember]
+        public String EMail { get; private set; }
+
+        [DataMember]
+        public String UserName { get; private set; }
+
+        [DataMember]
+        public String Password { get; private set; }
+
+        [DataMember]
+        public long TelephoneNumber { get; private set; }
+
+        [DataMember]
+        public String MothersMaiden { get; private set; }
+
+        [DataMember]
+        public DateTime BirthDay { get; private set; }
+
+        [DataMember]
+        public int Age { get; private set; }
+
+        [DataMember]
+        public String Occupation { get; private set; }
+
+        [DataMember]
+        public String Company { get; private set; }
+
+        [DataMember]
+        public String Vehicle { get; private set; }
+
+        [DataMember]
+        public String BloodType { get; private set; }
+
+        [DataMember]
+        public double Kilograms { get; private set; }
+
+        [DataMember]
+        public double Centimeters { get; private set; }
+
+        [DataMember]
+        public String Guid { get; private set; }
+
+        [DataMember]
+        public String Latitude { get; private set; }
+
+        [DataMember]
+        public String Longitude { get; private set; }
+
+        [DataMember]
+        public double Imc { get; private set; }
+
+        public override string ToString()
+        {
+            string bd = string.Empty;
+            bd += "Number: " + Number + Environment.NewLine;
+            bd +=  "Sex: " + Sexo + Environment.NewLine;
+            bd += "First Name: " + FirstName + Environment.NewLine;
+            bd += "Last Name: " +LastName + Environment.NewLine;
+            bd += "Street Address: " + StreetAddress +Environment.NewLine;
+            bd += "City: " + City + Environment.NewLine;
+            bd += "Statefull: " + Statefull + Environment.NewLine;
+            bd += "ZipCode: " + ZipCode + Environment.NewLine;
+            bd += "EMail: " + EMail + Environment.NewLine;
+            bd += "UserName: " + UserName + Environment.NewLine;
+            bd += "Password: " + Password + Environment.NewLine;
+            bd += "TelephoneNumber: " + TelephoneNumber + Environment.NewLine;
+            bd += "MothersMaiden: " + MothersMaiden + Environment.NewLine;
+            bd += "BirthDay: " + BirthDay + Environment.NewLine;
+            bd += "Age: " + Age + Environment.NewLine;
+            bd += "Occupation: " + Occupation + Environment.NewLine;
+            bd += "Company: " + Company + Environment.NewLine;
+            bd += "Vehicle: " + Vehicle + Environment.NewLine;
+            bd += "BloodType: " + BloodType + Environment.NewLine;
+            bd += "Kilograms: " + Kilograms + Environment.NewLine;
+            bd += "Centimeters: " + Centimeters + Environment.NewLine;
+            bd += "Guid: " + Guid + Environment.NewLine;
+            bd += "Latitude: " + Latitude + Environment.NewLine;
+            bd += "Longitude: " + Longitude + Environment.NewLine;
+            bd += "Imc: " + Imc + Environment.NewLine;
+           
+            return bd;
+        }
+
     }
+}
